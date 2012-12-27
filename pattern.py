fa501fc3a5a86302f09df5a77fe1ec9d823e4642
Li Zhao <iloahz@gmail.com>
2012-12-27 16:00:00 +0800

    added pattern interface

--- pattern.py
+++ pattern.py
@@ -1,4 +1,7 @@
+import webapp2
 from func import *
+from google.appengine.ext.webapp import template
+import os
 
 def validate(c):
     p = db.GqlQuery('SELECT * FROM Pattern WHERE input = :1', c).get()
@@ -10,3 +13,25 @@ def answer(ToUserName, FromUserName, CreateTime, MsgType, Content):
     p = db.GqlQuery('SELECT * FROM Pattern WHERE input = :1', Content).get()
     Content = p.output
     return genTextXml(ToUserName, FromUserName, CreateTime, MsgType, Content)
+
+def createOrUpdatePattern(i, o):
+    p = db.GqlQuery('SELECT * FROM Pattern WHERE input = :1', i).get()
+    if not p:
+        p = Pattern()
+        p.input = i
+    p.output = o
+    p.save()
+
+class PatternHandler(webapp2.RequestHandler):
+    def get(self):
+        path = os.path.join(os.path.dirname(__file__), 'template', 'pattern.html')
+        self.response.write(template.render(path, {}))
+    def post(self):
+        i = self.request.get('input')
+        o = self.request.get('output')
+        createOrUpdatePattern(i, o)
+        self.redirect('/pattern')
+
+app = webapp2.WSGIApplication([
+    ('/pattern', PatternHandler)
+], debug = True)
