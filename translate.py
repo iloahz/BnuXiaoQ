19ab14f204f54d7ff72aae050eaefae2de17c840
Li Zhao <iloahz@gmail.com>
2012-12-30 00:23:22 +0800

    shift from baidu to youdao

--- translate.py
+++ translate.py
@@ -1,14 +1,44 @@
+#-*- encoding: utf-8 -*-
+
 from func import *
 import urllib
 from google.appengine.api import memcache
 import json
 
 def validate(c):
-    if c.startswith('d ') or c.startswith('fy'):
+    if c.startswith('d ') or c.startswith('fy '):
         return True
     return False
 
-def answer(ToUserName, FromUserName, CreateTime, MsgType, Content, Simple = True):
+def answerYoudao(ToUserName, FromUserName, CreateTime, MsgType, Content):
+    keyword = ' '.join(Content.split()[1:])
+    Content = memcache.get(key = keyword, namespace = 'dict')
+    if not Content:
+        #get data from youdao
+        url = 'http://fanyi.youdao.com/openapi.do?'
+        arg = {
+            'keyfrom' : 'BnuXiaoQ',
+            'key' : '1336627529',
+            'type' : 'data',
+            'doctype' : 'json',
+            'version' : '1.1',
+            'q' : keyword
+        }
+        arg = urllib.urlencode(arg)
+        url += arg
+        res = urlfetch.fetch(url).content
+        j = json.loads(res)
+        Content = j['translation'][0]
+        try:
+            tmp = '释义：'
+            tmp += ','.join(j['basic']['explains'])
+            Content += '\n' + tmp
+        except Exception:
+            pass
+        memcache.add(key = keyword, value = Content, namespace = 'dict')
+    return genTextXml(ToUserName, FromUserName, CreateTime, MsgType, Content)
+
+def answerBaidu(ToUserName, FromUserName, CreateTime, MsgType, Content):
     keyword = ' '.join(Content.split()[1:])
     r = minidom.getDOMImplementation()
     d = r.createDocument(None, 'xml', None)
@@ -39,7 +69,7 @@ def answer(ToUserName, FromUserName, CreateTime, MsgType, Content, Simple = True
             'q' : keyword,
             'from' : 'auto',
             'to' : 'auto',
-        }
+            }
         arg = urllib.urlencode(arg)
         url += arg
         res = urlfetch.fetch(url).content
@@ -56,3 +86,6 @@ def answer(ToUserName, FromUserName, CreateTime, MsgType, Content, Simple = True
     x.appendChild(s)
     dat = x.toxml()
     return dat
+
+def answer(ToUserName, FromUserName, CreateTime, MsgType, Content):
+    return answerYoudao(ToUserName, FromUserName, CreateTime, MsgType, Content)
