5d6d24b798551709522bf0dbdc28a756a86c06a4
Li Zhao <iloahz@gmail.com>
2012-12-30 15:11:53 +0800

    remove last empty line

--- pattern.py
+++ pattern.py
@@ -24,7 +24,7 @@ def answer(ToUserName, FromUserName, CreateTime, MsgType, Content):
     p = db.GqlQuery('SELECT * FROM Pattern WHERE input = :1', Content).get()
     Content = p.output
     if p.hintNext:
-        Content += '\n你可以再试试“' + p.hintNext + '”哦~\n'
+        Content += '\n你可以再试试“' + p.hintNext + '”哦~'
     p.hintNext = getRandomPattern()
     if p.hintNext == p.input:
         p.hintNext = None
